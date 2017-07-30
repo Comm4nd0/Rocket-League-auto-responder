a91b2360ce077dce871bcb35cba4407ad0463973
Comm4ndo <marcobaldanza@hotmail.co.uk>
2017-07-30 22:29:39 +0100

    What a save!

--- rl-bot.py
+++ rl-bot.py
@@ -11,16 +11,21 @@ def send_command(key):
 
 while True:
     img = ImageGrab.grab(bbox=(5,5,600,300)).save('ss.jpg', 'JPEG')
-    #ImageGrab.grab_to_file('ss.png')
     all_text = image_to_string(Image.open('ss.jpg'))
     text = all_text.split('\n')
     for line in text:
         if 'YOU' not in line:
-            print(line)
             if 'Nice' in line:
+                print(line)
                 send_command('2')
                 send_command('3')
-            if 'Sorry' in line:
+            if 'Sorry' in line or 'bad' in line:
+                print(line)
                 send_command('4')
                 send_command('2')
+            if 'Great' in line:
+                print(line)
+                send_command('2')
+                send_command('3')
+
     time.sleep(2)
